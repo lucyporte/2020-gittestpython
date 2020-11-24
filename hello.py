bfc048c80eef563c2bd818e7f4eebc9cf76b5f99
g15550lp <lucy.porte@student.manchester.ac.uk>
2020-11-24 20:05:01 +0000

    Now adds the two numbers

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 #print("Hello World")
-num1 = input("Please enter the first number")
-num2 = input("Please enter the second number")
+num1 = float(input("Please enter the first number"))
+num2 = float(input("Please enter the second number"))
 print("You entered ", num1, "and",num2)
+sum = num1 + num2
+print("The sum of the two numbers entered is", sum)
